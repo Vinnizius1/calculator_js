fdc0af93a5dba4ed2a1355a98db96d9600332e2a
vinicius <vmm_7@hotmail.com>
2023-02-19 10:19:05 -0300

    1ª parte do JS: selecionando elementos do DOM

---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+const previousOperationText = document.querySelector("#previous-operation");
+const currentOperationText = document.querySelector("#current-operation");
+const buttons = document.querySelectorAll("#buttons-container button");
