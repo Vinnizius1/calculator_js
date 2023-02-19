7e75bf0173ba2a615f09d58cc25169b752c41067
vinicius <vmm_7@hotmail.com>
2023-02-19 10:51:00 -0300

    Separando números e ponto das operações

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
 const previousOperationText = document.querySelector("#previous-operation");
 const currentOperationText = document.querySelector("#current-operation");
 const buttons = document.querySelectorAll("#buttons-container button");
+
+/* Regra de Negócio */
+class Calculator {}
+
+/* Eventos de funcionamento da calculadora */
+buttons.forEach(btn => {
+  btn.addEventListener("click", e => {
+    const value = e.target.innerText;
+
+    if (+value >= 0 || value === ".") {
+      console.log("Número ou ponto: " + value);
+    } else {
+      console.log("Operação: " + value);
+    }
+  });
+});
